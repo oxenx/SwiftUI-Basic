cd7fd0850697f5931baf67c140a54c3e6f41dfb1
SeongKookKIM <ksk8646@naver.com>
2024-04-24 14:16:24 +0900

    Search Scope

--- SearchList/SearchList/ContentView.swift
+++ SearchList/SearchList/ContentView.swift
@@ -10,15 +10,28 @@ import SwiftUI
 
 
 struct ContentView: View {
+    enum FruitSearchScope: Hashable {
+        case all
+        case food(Food.Category)
+    }
+    
+    @State var scope: FruitSearchScope = .all
+    
     @State var searchText = ""
     let food: [Food] = Food.sampleFood
     
     var searchResults: [Food] {
-        if searchText.isEmpty {
-            return food
-        } else {
-            return food.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        var food: [Food] = self.food
+        
+        if case let .food(category) = scope {
+            food = food.filter { $0.category == category }
         }
+        
+        if !searchText.isEmpty {
+            food = food.filter { $0.name.lowercased().contains(searchText.lowercased())}
+        }
+        
+        return food
     }
     
     var body: some View {
@@ -31,8 +44,13 @@ struct ContentView: View {
                     
                 }
             }
-            
             .searchable(text: $searchText, prompt: "Search for a food")
+            .searchScopes($scope, activation: .onSearchPresentation) {
+                Text("All").tag(FruitSearchScope.all)
+                Text("Fruit").tag(FruitSearchScope.food(.fruit))
+                Text("Meat").tag(FruitSearchScope.food(.meat))
+                Text("Vegetable").tag(FruitSearchScope.food(.vegetable))
+            }
             .navigationTitle("Foods")
         }
         .textInputAutocapitalization(.never) // 키보드 첫문자 소문자로
