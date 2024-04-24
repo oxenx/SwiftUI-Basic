77e7c8d4e956402d3d74d9df1d3f58e6da38648c
SeongKookKIM <ksk8646@naver.com>
2024-04-24 09:33:39 +0900

    UIkit To SwiftUI

---
+++ SwiftUItoUIKit/SwiftUItoUIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftUItoUIKit
+//
+//  Created by SeongKook on 4/24/24.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBSegueAction func goToSwiftUI(_ coder: NSCoder) -> UIViewController? {
+        let greetings = "Hello From UIKit"
+        let rootView = Greetings(textFromUIKit: greetings)
+        return UIHostingController(coder: coder, rootView: rootView)
+    }
+    
+}
+
