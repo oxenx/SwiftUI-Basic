e4ef7d85274b5c8055555b7d2b745aeb5e46d46b
SeongKookKIM <ksk8646@naver.com>
2024-04-24 10:49:31 +0900

    StaticList, Data seperate

---
+++ StaticList/StaticList/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  StaticList
+//
+//  Created by SeongKook on 4/24/24.
+//
+
+import SwiftUI
+
+
+
+
+struct ContentView: View {
+    var weatherData: [WeatherInfo]
+    
+    var body: some View {
+        List {
+            ForEach(weatherData) { weather in
+                WeatherRow(weather: weather)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView(weatherData:  WeatherInfo.weatherData)
+}
