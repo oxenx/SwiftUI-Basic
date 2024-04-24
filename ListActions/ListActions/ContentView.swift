0c520184d695b6f044a8d267726650c8371f5f38
SeongKookKIM <ksk8646@naver.com>
2024-04-24 11:07:04 +0900

    List Add

---
+++ ListActions/ListActions/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  ListActions
+//
+//  Created by SeongKook on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var numbers = [1,2,3,4]
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(numbers, id: \.self) { number in
+                        Text("\(number)")
+                }
+            }
+            .navigationTitle("Number List")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button("Add") {
+                    addItemToRow()
+                }
+            }
+        }
+    }
+    
+    private func addItemToRow() {
+        numbers.append(Int.random(in: 5..<100))
+    }
+}
+
+#Preview {
+    ContentView()
+}
