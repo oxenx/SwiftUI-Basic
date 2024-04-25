6ca4181e0a4ed333361093f4af0009d488832674
SeongKookKIM <ksk8646@naver.com>
2024-04-25 09:27:43 +0900

    LazyGrid

---
+++ LazyGrid/LazyGrid/LazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridApp.swift
+//  LazyGrid
+//
+//  Created by mac on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
