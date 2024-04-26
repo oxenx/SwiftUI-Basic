a2185db6d8c619334b23b8d1217d7c1901830ac6
SeongKookKIM <ksk8646@naver.com>
2024-04-26 17:13:11 +0900

    Login UI

---
+++ FormFieldDisabled/FormFieldDisabled/FormFieldDisabledApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormFieldDisabledApp.swift
+//  FormFieldDisabled
+//
+//  Created by mac on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FormFieldDisabledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FormFieldDisabled/FormFieldDisabled/LoginView.swift
@@ -0,0 +1,51 @@
+//
+//  LoginView.swift
+//  FormFieldDisabled
+//
+//  Created by mac on 4/26/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var userName = ""
+    @State private var password = ""
+    
+    var body: some View {
+        VStack {
+            Text("Dungeons and Wagons")
+                .fontWeight(.heavy)
+                .foregroundStyle(.blue)
+                .font(.largeTitle)
+                .padding(.bottom, 30)
+                
+            Image(systemName: "person.circle")
+                .font(.system(size: 150))
+                .foregroundStyle(.gray)
+                .padding(.bottom, 40)
+            
+            Group {
+                TextField("UserName", text: $userName)
+                SecureField("Password", text: $password)
+            }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.black, lineWidth: 2))
+            
+            Button {
+                print("Login tapped")
+            } label: {
+                Text("Login")
+            }
+            .padding()
+            .background((userName.isEmpty || password.isEmpty) ? .gray : .blue)
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+            .disabled(userName.isEmpty || password.isEmpty ? true : false)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
