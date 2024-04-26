402b7825c704fdfc12477218b66df5e77da32da5
SeongKookKIM <ksk8646@naver.com>
2024-04-26 16:33:50 +0900

    Form SignUp Create

---
+++ SignUp/SignUp/SignUpView.swift
@@ -0,0 +1,70 @@
+//
+//  SignUpView.swift
+//  SignUp
+//
+//  Created by mac on 4/26/24.
+//
+
+import SwiftUI
+
+struct Address {
+    var street: String = ""
+    var city: String = ""
+    var postalCode: String = ""
+}
+
+
+struct SignUpView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var address = Address()
+    @State private var address2 = Address()
+    @State private var lessThenTwo = false
+    @State private var userName = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Names") {
+                    TextField("Frist Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                }
+                
+                Section("Current Address") {
+                    TextField("Street Address", text: $address.street)
+                    TextField("City", text: $address.city)
+                    TextField("PostalCode", text: $address.postalCode)
+                    Toggle(isOn: $lessThenTwo) {
+                        Text("Have you lived here for 2+ year")
+                    }
+                }
+                
+                if !lessThenTwo {
+                    Section("Previous Address") {
+                        TextField("Street Address", text: $address2.street)
+                        TextField("City", text: $address2.city)
+                        TextField("PostalCode", text: $address2.postalCode)
+                    }
+                }
+                
+                Section {
+                    TextField("Create UserName", text: $userName)
+                    SecureField("Create Password", text: $password)
+                }
+                
+                Button("Submit") {
+                    print("Form submit action here")
+                }
+                
+                
+            }
+            .navigationTitle("Sign Up")
+        }
+    }
+}
+
+#Preview {
+    SignUpView()
+}
