5598ae404f029859bb7d6c8ed14aa7e89d884de4
SeongKookKIM <ksk8646@naver.com>
2024-04-26 11:47:59 +0900

    Preview with Traits

---
+++ PreviewingWithTraits/PreviewingWithTraits/PreviewingWithTraitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewingWithTraitsApp.swift
+//  PreviewingWithTraits
+//
+//  Created by mac on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PreviewingWithTraitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
