ddf1c318e8f13282347e57c00c0f13a37eccc949
SeongKookKIM <ksk8646@naver.com>
2024-04-29 15:44:27 +0900

    Login losic

---
+++ FirebaseBasic/FirebaseBasic/AuthenticationViewModel.swift
@@ -0,0 +1,82 @@
+//
+//  AuthenticationViewModel.swift
+//  FirebaseBasic
+//
+//  Created by mac on 4/29/24.
+//
+
+import Foundation
+import Firebase
+import GoogleSignIn
+import FirebaseCore
+
+extension UIApplication {
+    static var currentRootViewController: UIViewController? {
+        UIApplication.shared.connectedScenes
+            .filter({ $0.activationState == .foregroundActive })
+            .map({ $0 as? UIWindowScene })
+            .compactMap({ $0 })
+            .first?.windows
+            .filter({ $0.isKeyWindow })
+            .first?
+            .rootViewController
+    }
+}
+
+@Observable
+final class AuthenticationViewModel {
+    enum State {
+        case busy
+        case signedIn
+        case signedOut
+    }
+    
+    var state: State = .busy
+    
+    private var authResult: AuthDataResult? = nil
+    
+    var userName: String { authResult?.user.displayName ?? "" }
+    var userEmail: String { authResult?.user.email ?? "" }
+    var usephotoURL: URL? { authResult?.user.photoURL }
+    
+    func logOut() {
+        
+    }
+    
+    func restorePreviousSignIn() {
+        
+    }
+    
+    func login() {
+        state = .busy
+        guard let clientID = FirebaseApp.app()?.options.clientID,
+              let rootViewController = UIApplication.currentRootViewController else {
+            return
+        }
+        
+        let configuration = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = configuration
+        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController, hint: nil) { result, error in
+            if let error { print("Error: \(error.localizedDescription)")}
+            Task {
+                await self.signIn(user: result?.user)
+            }
+        }
+    }
+    
+    private func signIn(user: GIDGoogleUser?) async {
+        guard let user, let idToken = user.idToken else {
+            state = .signedOut
+            return
+        }
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken.tokenString, accessToken: user.accessToken.tokenString )
+        
+        do {
+            authResult = try await Auth.auth().signIn(with: credential)
+            state = .signedIn
+        } catch {
+            state = .signedOut
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+}
