b91fe6a6e58b464eeb1732ad92deffe58bf3dacc
SeongKookKIM <ksk8646@naver.com>
2024-04-29 10:36:26 +0900

    @AppStorage && 로그인시 데이터저장

--- SignInWithApple/SignInWithApple/ContentView.swift
+++ SignInWithApple/SignInWithApple/ContentView.swift
@@ -11,7 +11,19 @@ import AuthenticationServices
 struct ContentView: View {
     
     @State private var userName: String = ""
-    @State private var useEmail: String = ""
+    @State private var userEmail: String = ""
+    
+    @AppStorage("storedName") private var storedName: String = "" {
+        didSet {
+            userName = storedName
+        }
+    }
+    @AppStorage("storeDEmail") private var storedEmail: String = "" {
+        didSet {
+            userEmail = storedEmail
+        }
+    }
+    @AppStorage("userID") private var userId: String = ""
     
     var body: some View {
         ZStack {
@@ -21,12 +33,37 @@ struct ContentView: View {
                     .signInWithAppleButtonStyle(.black)
                     .frame(width: 200, height: 50)
             } else {
-                Text("Welcome\n\(userName), \(useEmail)")
+                Text("Welcome\n\(userName), \(userEmail)")
                     .foregroundStyle(.black)
                     .font(.headline)
             }
         }
+        .task {
+            await authorize()
+        }
     }
+    
+    private func authorize() async {
+        guard !userId.isEmpty else {
+            userName = ""
+            userEmail = ""
+            return
+        }
+        guard let credentialState = try? await ASAuthorizationAppleIDProvider().credentialState(forUserID: userId) else {
+            userName = ""
+            userEmail = ""
+            return
+        }
+        switch credentialState {
+        case .authorized:
+            userName = storedName
+            userEmail = storedEmail
+        default:
+            userName = ""
+            userEmail = ""
+        }
+    }
+    
     private func onRequest(_ request: ASAuthorizationAppleIDRequest) {
         request.requestedScopes = [.fullName, .email]
     }
@@ -36,6 +73,11 @@ struct ContentView: View {
         case .success(let authResult):
             guard let credential = authResult.credential as? ASAuthorizationAppleIDCredential else { return }
         // Todo: stroe Data
+            storedName = credential.fullName?.givenName ?? ""
+            storedEmail = credential.email ?? ""
+            userId = credential.user
+            
+            
         case .failure(let error):
             print("Authorization failed: " + error.localizedDescription)
         }
